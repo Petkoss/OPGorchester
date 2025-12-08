9aa58df08d27f7640b80cb6bc36d91ed98e32d54
Kristián Eliaš <kristiandeqfo@gmail.com>
2025-12-08 10:23:08 +0100

    Pridane veci do class StrunovyNastroj

---
+++ StrunovyNastroj.java
@@ -0,0 +1,40 @@
+package OPGorchester;
+
+public class StrunovyNastroj extends Nastroje {
+    int pocetStrun;
+    String ladenie;
+    public StrunovyNastroj(String druh, double cena, String zvuk, int pocet, int pocetStrun, String ladenie) {
+        super(druh, cena, zvuk, pocet);
+        this.pocetStrun = pocetStrun;
+        this.ladenie = ladenie;
+    }
+
+    public int getPocetStrun() {
+        return pocetStrun;
+    }
+
+    public String getLadenie() {
+        return ladenie;
+    }
+    
+    public void setPocetStrun(int pocetStrun) {
+        if (pocetStrun > 0) {
+            this.pocetStrun = pocetStrun;
+        } else {
+            System.out.println("Nemoze byt mensie ako 1");
+        }
+    }
+
+    public void setLadenie(String ladenie) {
+        if (ladenie == "") {
+            System.out.println("Nemoze byt prazdny");;
+        } else {
+            this.ladenie = ladenie;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "StrunovyNastroj [" + super.toString() + ", pocetStrun=" + pocetStrun + ", ladenie=" + ladenie + "]";
+    }
+}
