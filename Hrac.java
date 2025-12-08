32d53953e92b30b3d641767c6632a8e76012905c
Petkoss <bednarp@spse-po.sk>
2025-12-08 10:10:15 +0100

    class hrac

---
+++ Hrac.java
@@ -0,0 +1,14 @@
+package ORCHESTER;
+
+public class Hrac{
+    public String meno;
+    public String priezvisko;
+    public String nastroj;
+    public int hodinovaSadzba;
+    public Hrac(String meno, String priezvisko, String nastroj, int hodinovaSadzba){
+        this.meno = meno;
+        this.priezvisko = priezvisko;
+        this.nastroj = nastroj;
+        this.hodinovaSadzba = hodinovaSadzba;
+    }
+}
