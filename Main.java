03f5b9c9e96b4cbeda1598beafd72d15f0fc353f
Petkoss <bednarp@spse-po.sk>
2025-12-15 11:10:00 +0100

    branch

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        try (BufferedReader br = new BufferedReader(  new FileReader("OPGorchester/skladNastrojov.txt"))) {
+        try (BufferedReader br = new BufferedReader(  new FileReader("ORCHESTER/OPGorchester/skladNastrojov.txt"))) {
             ArrayList<Nastroj> nastroje = new ArrayList<>();
             ArrayList<Hrac> umelci = new ArrayList<>();
             String line;
